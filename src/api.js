5c0889084914a3f9daf61be49b9c5fa62504cec9
VadimRydenko <rydenkovvv@gmail.com>
2021-02-15 11:22:10 +0200

    remove octokit and add axios

--- src/api.js
+++ src/api.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import {Octokit} from '@octokit/rest';
+import axios from 'react-native-axios';
 
 import {GIT_HUB_TOKEN} from './const';
 
-const octokit = new Octokit({
-  auth: GIT_HUB_TOKEN,
-});
+// const octokit = new Octokit({
+//   auth: GIT_HUB_TOKEN,
+// });
+
+// export const getUserList = async () => {
+//   const response = await octokit.request(
+//     'https://api.github.com/users?since=1&per_page=100',
+//   );
+//   return response.data;
+// };
+
+// export const getFollowers = async (userName) => {
+//   const response = await octokit.request(
+//     `https://api.github.com/users/${userName}/followers`,
+//   );
+//   return response.data;
+// };
 
 export const getUserList = async () => {
-  const response = await octokit.request(
+  const response = await axios.get(
     'https://api.github.com/users?since=1&per_page=100',
   );
   return response.data;
 };
 
 export const getFollowers = async (userName) => {
-  const response = await octokit.request(
+  const response = await axios.get(
     `https://api.github.com/users/${userName}/followers`,
   );
   return response.data;
 };
-
